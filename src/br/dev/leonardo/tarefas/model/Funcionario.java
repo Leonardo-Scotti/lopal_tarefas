d3de12cf67a9b6613a22f6815e429ba39f70a7d3
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-05-19 16:20:06 -0300

    Método Contrutor

---
+++ src/br/dev/leonardo/tarefas/model/Funcionario.java
@@ -0,0 +1,53 @@
+package br.dev.leonardo.tarefas.model;
+
+public class Funcionario {
+	private String nome;
+	private int matricula;
+	private String cargo;
+	
+	//Método construtor
+	public Funcionario() {
+		
+	}
+	
+	public Funcionario(String nome) {
+		this.nome = nome;
+	}
+	
+	public Funcionario(String cargo, String nome) {
+		this.cargo = cargo;
+		this.nome = nome;
+	}
+	
+	public Funcionario(String cargo, String nome, int matricula) {
+		this.cargo = cargo;
+		this.nome = nome;
+		this.matricula = matricula;
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+}
